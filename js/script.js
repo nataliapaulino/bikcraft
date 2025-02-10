33c39fb429af984edd4b1fea0747d7516a330e61
Natália Paulino <npalbuquerque@outlook.com>
2025-02-10 15:48:27 -0300

    add query strings to enable product auto-selection

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-const links =document.querySelectorAll('.header-menu a');
+// Ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  if (url.includes(href)){
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
